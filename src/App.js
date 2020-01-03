2a2e1ee549009fe0177ab01a6b0913b42212aa8a
Haluskua <haluskua.one@gmail.com>
2020-01-03 15:58:18 +1300

    included code for dealing with race condition by using cancelToken by axios

--- src/App.js
+++ src/App.js
@@ -12,13 +12,17 @@ function App() {
 
   useEffect(() => {
     setLoading(true)
-    Axios.get(currentPageUrl).then(res => {
+    let cancel 
+    Axios.get(currentPageUrl, {
+      cancelToken: new Axios.CancelToken(c => cancel = c)
+    }).then(res => {
       setLoading(false)
       setNextPageUrl(res.data.next)
       setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
     })
-  
+
+    return () => cancel()
   }, [currentPageUrl])
 
   if (loading) return "Loading..."
