e36bd7ff7ccf4dfe7e1bdc27c9a1edc1f552eea9
Haluskua <haluskua.one@gmail.com>
2020-01-03 15:10:53 +1300

    accessing Pokemon Api without useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import PokemonList from "./PokemonList";
+import Axios from 'axios';
 function App() {
+  const [pokemon, setPokemon ] = useState([])
+
+  Axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+    setPokemon(res.data.results.map(p => p.name))
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <PokemonList pokemon={pokemon}/>
+
   );
 }
 
