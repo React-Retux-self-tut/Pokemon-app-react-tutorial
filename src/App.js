9dabe1e474d71ca68abb56cc8f65b68928c8281a
Haluskua <haluskua.one@gmail.com>
2020-01-03 15:40:35 +1300

    using axios to fech api with useEffect plus loading condition without race contidion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PokemonList from "./PokemonList";
 import Axios from 'axios';
 function App() {
   const [pokemon, setPokemon ] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
 
-  Axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
-    setPokemon(res.data.results.map(p => p.name))
-  })
+
+
+  useEffect(() => {
+    setLoading(true)
+    Axios.get(currentPageUrl).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+  
+  }, [currentPageUrl])
+
+  if (loading) return "Loading..."
+
 
   return (
    <PokemonList pokemon={pokemon}/>
